01e179d2431f6dee760f65d5009dfc8e00ecf955
Pujan <pujan007mm@gmail.com>
2022-01-20 22:10:10 +0530

    bg fix

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import backgroundImg from "../assets/background.png";
+import '../css/Homepage.css';
 export default function Homepage() {
     return (
-        <div>
-            <img src={backgroundImg} style={{'position':'relative'}}></img>
+        <div ClassName="homepage">
+            <img ClassName="backgroundimg" src={backgroundImg}></img>
         </div>
     )
 }
