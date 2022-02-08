3577803fce262fd27467c479ba346695d57b3ef2
Pujan <pujan007mm@gmail.com>
2022-02-08 14:39:32 +0530

    try fix 2

--- frontend/src/UserComponents/Header.js
+++ frontend/src/UserComponents/Header.js
@@ -37,7 +37,7 @@ export default function Header(){
           });
         };
         let responseData = await response();
-        setUser(responseData.data.user);
+        setUser(responseData.data);
       })();
 	},[]);
 
