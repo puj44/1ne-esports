d706b925620b5abd0bad9271e1930925597557c1
drachit631 <drachit631@gmail.com>
2022-05-05 16:51:45 +0530

    resolve events and game night issues

--- frontend/src/AdminComponents/SidebarComponents/SidebarComunityGameNight.js
+++ frontend/src/AdminComponents/SidebarComponents/SidebarComunityGameNight.js
@@ -1,7 +1,153 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Modal from 'react-bootstrap/Modal'
+import Alert from '../Alert';
+import axios from 'axios';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+import '../../css/gamenight.css';
 
 export default function SidebarComunityGameNight() {
+const [inputerror,setinputerror]=useState('');
+const [show,setShow]=useState(false);
+const [allCGN,setallCGN]=useState('');
+const [isfetched,setfetched]=useState(false);
+const [newCGN,setnewCGN]=useState('');
+const [popUp,setPopUp]=useState(false);
+const [title,settitle]= useState('');
+const [date,setdate]= useState('');
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+const handlePopUpClose = () => setPopUp(false);
+//const handlePopUpShow = () => setPopUp(true);
+    const handleAddCGEvent=(e)=>{
+      e.preventDefault();
+      axios({
+        method: 'POST',
+        url: 'http://localhost:3000/admin/add/CG',
+        data: {
+          'title' : newCGN.title,
+          'date' : newCGN.date
+        },
+          withCredentials: true,
+          credentials: "include",
+        }).then((response) => {
+          if(response.status===200)
+          handleClose();
+        },(error) => {
+          if(error.response!==undefined){
+          if(error.response.status===403){
+            setinputerror('Could not add!');
+          }
+          if(error.response.status===400){
+            setinputerror('Login!');
+          }
+          if(error.response.status===402){
+            setinputerror('Length Exceeded!');
+          }       
+        }
+      });
+      setShow(false);
+      console.log(newCGN);
+    }
+
+    const fetch=()=>{
+      axios({
+        method: 'get',
+        url: 'http://localhost:3000/admin/CG/all',
+        withCredentials: true,
+        credentials: "include",
+        }).then((response) => {
+        if(response.status===200){
+          setallCGN(response.data.cgnResult);
+        }
+      }, (error) => {
+          if(error.response.status===400){
+            setinputerror('Could not load');
+          }
+          else{
+            setinputerror(error.response.message);
+          }
+      });
+     setfetched(true);
+  }
+    
+  const popup=(date)=>{
+    Object.keys(allCGN).forEach((element) => {
+      if (allCGN[element].date===moment(date).format('DD-MM-YYYY'))
+      {
+        settitle(allCGN[element].title);
+        setdate(allCGN[element].date);
+        setPopUp(true);
+      }
+    });
+  }
+
+if(isfetched===false){
+    fetch();
+    //console.log(mark);
+  }
   return (
-    <div>SidebarComunityGameNight</div>
+    <div className="flex-container gameElement desktopElement">
+
+      <div className="CG" >
+       <div style={{"marginLeft":"50%"}}>
+          <button  className="btn"  style={{"backgroundColor":"#343a40","color":"white","marginTop":"3%","width":"40%"}} onClick={()=>handleShow()}>Add Schedule</button>
+        </div> 
+        <Modal show={show} onHide={handleClose}>
+          <form>
+            <Modal.Header closeButton >
+              <Modal.Title >Add Schedule for Game Night</Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={{"backgroundColor":"black","fontSize":"22px"}}>
+              <h1 className='display-7' style={{'color':'white'}}>1<span style={{'color':'yellow'}}>N</span>E Esports</h1>
+              <br/>
+              <input 
+                style={{"fontSize":"22px"}} 
+                className='form-control shadow p-2 bg-body rounded' 
+                placeholder='Event Name' 
+                id='eventName' 
+                type='text' 
+                value={newCGN.title} 
+                onChange={(e) => setnewCGN({...newCGN,title:e.target.value})}
+              />
+              <br/>
+
+              <div style={{display:'flex',flexDirection:'row'}}>
+                <DatePicker 
+                  className="form-control shadow p-2 bg-body rounded"
+                  placeholderText="Date" 
+                  selected={newCGN.date} 
+                  onChange={(date)=> setnewCGN({...newCGN,date})}
+                /> 
+              </div>
+              {inputerror === ''?null:<Alert message={inputerror} type='danger'/>}
+            </Modal.Body>  
+            <Modal.Footer>
+            <center>
+              <button tag='input'  className='btn btn-primary fs-4 w-auto h-auto' onClick={(e)=>handleAddCGEvent(e)}>Add</button>
+            </center>   
+            </Modal.Footer>
+          </form>
+        </Modal>
+      </div>
+
+      <div className="Calendar" style={{"marginTop":"2%","marginLeft":"15%"}}>
+        <Calendar style={{"height":"50%"}}
+          onChange= {(date)=> popup(date)}
+          tileClassName={({date,view})=>Object.keys(allCGN).map((item)=>{ return(
+            allCGN.find(x=>x.date===moment(date).format('DD-MM-YYYY'))?'highlight':''
+          )})}
+          defaultView='month'
+        />
+      </div>
+      <Modal class="popup" show={popUp} onHide={handlePopUpClose}>
+        <Modal.Header closeButton >
+        <Modal.Title ><p style={{"margin":"0.9%","width":"100%","fontSize":"28px"}}>Game Night "{title}"</p><p style={{"margin":"0.9%","width":"100%","fontSize":"28px"}}>Date: {date}</p></Modal.Title>
+        </Modal.Header>
+      </Modal>
+    </div>
   )
 }
--- frontend/src/AdminComponents/SidebarComponents/SidebarEvents.js
+++ frontend/src/AdminComponents/SidebarComponents/SidebarEvents.js
@@ -1,75 +1,104 @@
-import React,{useState,useRef} from 'react';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import React,{useState,useEffect} from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import axios from 'axios';
 import Modal from 'react-bootstrap/Modal'
 import Alert from '../Alert';
+import axios from 'axios';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import moment from 'moment';
+import '../../css/gamenight.css';
 
-const locales={
-  "en-US": require("date-fns/locale/en-US")
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales
-})
-
-const events = [
-  {
-    title : "Community Game Night",
-    start: new Date(2022,2,14),
-    end : new Date(2022,2,22),
-  }
-]
 export default function SidebarEvents() {
-  const [newEvent,setNewEvent]=useState({title:"",start:"",end:""})
-  const [allEvents,setAllEvents]=useState(events);
-  const [inputerror,setinputerror]=useState('');
-  const [show,setShow]=useState(false);
-  // const flag=useRef(0);
-  // const handleChange=(title1,start1,end1)=>{
-  //   console.log(title1,start1,end1);
-  //   if(title1!==null){
-  //     setNewEvent(newEvent=>[...newEvent,{title:title1}]);
-  //   }
-  //   else if(start1!==null){
-  //     setNewEvent(newEvent=>[...newEvent,{start:start1}]);
-  //   }
-  //   else{
-  //     setNewEvent(newEvent=>[...newEvent,{end:end1}]);
-  //   }
-  // }
-  const handleAddEvent=(e)=>{
-    e.preventDefault();
-    setAllEvents([...allEvents,newEvent]);
+const [inputerror,setinputerror]=useState('');
+const [show,setShow]=useState(false);
+const [allEvents,setallEvents]=useState('');
+const [isfetched,setfetched]=useState(false);
+const [newEvent,setnewEvent]=useState('');
+const [popUp,setPopUp]=useState(false);
+const [title,settitle]= useState('');
+const [date,setdate]= useState('');
+const [time,settime]= useState('');
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+const handlePopUpClose = () => setPopUp(false);
+const handleAddCGEvent=(e)=>{
+  e.preventDefault();
+  axios({
+    method: 'POST',
+    url: 'http://localhost:3000/admin/add/event',
+    data: {
+      'title' : newEvent.title,
+      'date' : newEvent.date,
+    },
+    withCredentials: true,
+    credentials: "include",
+    }).then((response) => {
+      if(response.status===200)
+        handleClose();
+      },(error) => {
+        if(error.response!==undefined){
+          if(error.response.status===403){
+            setinputerror('Could not add!');
+          }
+          if(error.response.status===400){
+            setinputerror('Login!');
+          }
+          if(error.response.status===402){
+            setinputerror('Length Exceeded!');
+          }       
+        }
+      });
     setShow(false);
     console.log(newEvent);
+}
+const fetch=()=>{
+  axios({
+    method: 'GET',
+    url: 'http://localhost:3000/admin/event/all',
+    withCredentials: true,
+    credentials: "include",
+  }).then((response) => {
+    if(response.status===200){
+      setallEvents(response.data.eventResult);
+    }
+  }, (error) => {
+    if(error.response.status===400){
+      setinputerror('Could not load');
+    }
+    else{
+      setinputerror(error.response.message);
+    }
+  });
+  setfetched(true);
+}
     
+const popup=(date)=>{
+    Object.keys(allEvents).forEach((element) => {
+      if (allEvents[element].date===moment(date).format('DD-MM-YYYY'))
+      {
+        settitle(allEvents[element].title);
+        setdate(allEvents[element].date);
+        settime(allEvents[element].time);
+        setPopUp(true);
+      }
+    });
   }
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+if(isfetched===false){
+    fetch();
+  }
   return (
-    <div className="flex-container">
+    <div className="flex-container gameElement desktopElement">
 
-      <div className="Event" >
-        <center>
-          <button  className="btn"  style={{"backgroundColor":"#343a40","color":"white","marginTop":"3%","width":"10%"}} onClick={()=>handleShow()}>Add Event</button>
-        </center>
+      <div className="CG" >
+       <div style={{"marginLeft":"50%"}}>
+          <button  className="btn"  style={{"backgroundColor":"#343a40","color":"white","marginTop":"3%","width":"40%"}} onClick={()=>handleShow()}>Add Event</button>
+        </div> 
         <Modal show={show} onHide={handleClose}>
           <form>
             <Modal.Header closeButton >
-              <Modal.Title >Add Event</Modal.Title>
+              <Modal.Title >Community Events</Modal.Title>
             </Modal.Header>
             <Modal.Body style={{"backgroundColor":"black","fontSize":"22px"}}>
               <h1 className='display-7' style={{'color':'white'}}>1<span style={{'color':'yellow'}}>N</span>E Esports</h1>
@@ -81,48 +110,45 @@ export default function SidebarEvents() {
                 id='eventName' 
                 type='text' 
                 value={newEvent.title} 
-                onChange={(e) => setNewEvent({...newEvent,title:e.target.value})}
+                onChange={(e) => setnewEvent({...newEvent,title:e.target.value})}
               />
               <br/>
 
               <div style={{display:'flex',flexDirection:'row'}}>
                 <DatePicker 
                   className="form-control shadow p-2 bg-body rounded"
-                  placeholderText="Start Date" 
-                  selected={newEvent.start} 
-                  onChange={(start)=> setNewEvent({...newEvent,start})}
-                  showTimeSelect
+                  placeholderText="Date" 
+                  selected={newEvent.date} 
                   dateFormat="MM/dd/yyyy  EE hh:mm aa"
-                />&nbsp;&nbsp;
-                <DatePicker 
-                  className="form-control shadow p-2 bg-body rounded"
-                  placeholderText="End Date" 
-                  selected={newEvent.end} 
-                  onChange={(end)=> setNewEvent({...newEvent,end})}
                   showTimeSelect
-                  dateFormat="MM/dd/yyyy  EE hh:mm aa"
+                  onChange={(date)=> setnewEvent({...newEvent,date})}
                 /> 
               </div>
               {inputerror === ''?null:<Alert message={inputerror} type='danger'/>}
             </Modal.Body>  
             <Modal.Footer>
             <center>
-              <button tag='input'  className='btn btn-primary fs-4 w-auto h-auto' onClick={(e)=>handleAddEvent(e)}>Add</button>
+              <button tag='input'  className='btn btn-primary fs-4 w-auto h-auto' onClick={(e)=>handleAddCGEvent(e)}>Add</button>
             </center>   
             </Modal.Footer>
           </form>
         </Modal>
       </div>
 
-      <div className="Calendar" style={{"marginTop":"2%","height":500,"marginRight":"1%","backgroundColor": "white"}}>
-        <Calendar style={{"margin":"2%"}}
-          localizer={localizer} 
-          events={allEvents} 
-          startAccessor="start"
-          endAccessor="end"
+      <div className="Calendar" style={{"marginTop":"2%","marginLeft":"15%"}}>
+        <Calendar style={{"height":"50%"}}
+          onChange= {(date)=> popup(date)}
+          tileClassName={({date,view})=>Object.keys(allEvents).map((item)=>{ return(
+            allEvents.find(x=>x.date===moment(date).format('DD-MM-YYYY'))?'highlight':''
+          )})}
+          defaultView='month'
         />
       </div>
-
+      <Modal class="popup" show={popUp} onHide={handlePopUpClose}>
+        <Modal.Header closeButton >
+        <Modal.Title ><p style={{"margin":"0.9%","width":"100%","fontSize":"28px"}}>Community Event "{title}" <br/>Date: {date} &nbsp; Time: {time}</p></Modal.Title>
+        </Modal.Header>
+      </Modal>
     </div>
   )
 }
