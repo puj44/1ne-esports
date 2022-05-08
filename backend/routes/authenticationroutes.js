b659c5487af07d2a04aa66b2fa10797eceb51e34
drachit631 <drachit631@gmail.com>
2022-05-08 16:27:41 +0530

    done commit

--- backend/routes/authenticationroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../controllers/authenticationcontroller');
-const verifyToken = require('./verifyToken');
-router.get("/signout",verifyToken,auth.signout);
-router.get("/getstatus",auth.checkstatus);
-router.post("/signin/",auth.authenticate);
-module.exports= router;
